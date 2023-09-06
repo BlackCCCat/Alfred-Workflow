7f1198ee5abdc703bae986439b1c0d879e35fd33
BlackCCCat <360252749zzx@gmail.com>
2023-09-07 00:04:32 +0800

    add safari bookmarks workflow

---
+++ safari-bookmarks/README.md
@@ -0,0 +1,10 @@
+# Safari Bookmarks
+快速查看Safari的书签，包括Bookmarks、Favorites
+# **使用方式**
+1. 安装Workflow
+2. 首次`safari bks`调用Workflow，会提示书签文件不存在，直接回车打开书签需要放置的路径
+3. 手动从Safari导出书签到本Workflow所在的路径（上一步打开的路径）
+4. 书签文件为.html格式，最好命名为`Safari Bookmarks.html`，如果要命名为其他名称，请在Workflow第一个组件中更改传入的参数名称为你所更改的名称
+
+
+> 写好后发现Alfred自带有书签查看功能，不过还是有一点区别，该Workflow可以直接显示所有书签
---
+++ safari-bookmarks/bookmarks.py
@@ -0,0 +1,80 @@
+from bs4 import BeautifulSoup
+import os
+import sys
+import json
+import re
+
+
+class BookMarks():
+    def __init__(self, file):
+        self.file = file
+        self.res = dict()
+    
+    def getBookmarks(self):
+        with open(self.file, 'r') as bks:
+            bks_str = bks.read()
+        soup = BeautifulSoup(bks_str, 'html.parser')
+        a_tags = soup.find_all('a')
+        for a_tag in a_tags:
+            link = a_tag['href']
+            _title = a_tag.text
+            self.res[_title] = link
+        return self.res
+
+    def show(self, data):
+        items = []
+        for _title in data:
+            link = data[_title]
+    
+            line = {
+                'type':'default',
+                'title':_title,
+                'subtitle':f'Bookmark:{link}',
+                'arg':link
+            }
+            
+            if sys.argv[2]:
+                target = sys.argv[2]
+                if re.search(target, _title, flags=re.IGNORECASE):
+                    items.append(line)
+                elif re.search(target, link, flags=re.IGNORECASE):
+                    items.append(line)
+            else:
+                items.append(line)
+        result_json = json.dumps({"items":items}, ensure_ascii=False)
+        return result_json
+
+
+
+def showNoneFileInfo(wk_path, bk_path):
+    items = [
+        {
+         'type':'default',
+         'title':f"Bookmarks file doesn't exists, please export Bookmarks to current path",
+         'subtitle':f'Enter ↵ to open current directory:{wk_path}',
+         'arg':wk_path
+        }
+    ]
+    
+    result_json = json.dumps({"items":items}, ensure_ascii=False)
+    return result_json
+    
+
+
+def main():
+    current_dir = os.getcwd()
+    file_name = sys.argv[1] # Safari Bookmarks.html
+    html_dir = os.path.join(current_dir, file_name)
+    exists = os.path.exists(html_dir)
+    if exists:
+        bk = BookMarks(html_dir)
+        data = bk.getBookmarks()
+        result_json = bk.show(data)
+    else:
+        result_json = showNoneFileInfo(current_dir, html_dir)
+    
+    print(result_json)
+
+
+if __name__ == "__main__":
+    main()
