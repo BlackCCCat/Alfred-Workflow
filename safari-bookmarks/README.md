f24af7d7b64b0dd07d923538624ec3e23c7bbde3
BlackCCCat <360252749zzx@gmail.com>
2023-09-07 20:26:53 +0800

    add safari history

--- safari-bookmarks/README.md
+++ safari-bookmarks/README.md
@@ -1,13 +1,28 @@
-# Safari Bookmarks
+# Safari Bookmarks and History
+**本Workflow需要Alfred添加访问所有磁盘权限**
+
+## Bookmarks
 快速查看Safari的书签，包括Bookmarks、Favorites
 
-**需要Alfred添加访问所有磁盘权限**
 
-# **使用方式**
+## **使用方式**
 1. 安装Workflow
 2. `sbs`调用Workflow，显示所有Bookmarks及Favorites
 3. 如果Safari浏览器中未添加任何Bookmarks或Favorites，回车打开Safari浏览器
 4. `sbs [str]`可选参数，进行对输入的`str`进行模糊搜索以定位需要找到的书签
+
+## History
+快速查看Safari的历史浏览记录
+
+## **使用方式**
+1. 安装Workflow
+2. `shs`调用Workflow，显示所有History
+3. `shs [str]`可选参数，进行对输入的`str`进行模糊搜索以定位需要找到的历史记录
+
+
+# 其他
+- 所用基本都是Python自带的包
+- 需下载`sqlite3`
 
 
 > 写好后发现Alfred自带有书签查看功能，不过还是有一点区别，该Workflow可以直接显示所有书签
---
+++ safari-bookmarks/history.py
@@ -0,0 +1,126 @@
+import sqlite3
+import os
+import shutil
+import sys
+import datetime
+import re
+import json
+
+
+class History():
+
+    @staticmethod
+    def sql(db: str) -> list:
+        """
+        Executes SQL depending on History path
+        provided in db: str
+
+        Args:
+            db (str): Path to History file
+
+        Returns:
+            list: result list of dictionaries (Url, Title, VisiCount)
+        """
+        res = []
+        history_db = os.getcwd()
+
+        try:
+            shutil.copy2(db, history_db)
+            c = sqlite3.connect(os.path.join(history_db, "History.db"))
+            cursor = c.cursor()
+            # SQL satement for Safari
+            if "Safari" in db:
+                select_statement = f"""
+                    SELECT history_items.url, history_visits.title, history_items.visit_count,(history_visits.visit_time + 978307200)
+                    FROM history_items
+                        INNER JOIN history_visits
+                        ON history_visits.history_item = history_items.id
+                    WHERE history_items.url IS NOT NULL AND
+                        history_visits.TITLE IS NOT NULL AND
+                        history_items.url != '' order by visit_time DESC
+                """
+            # SQL statement for Chromium Brothers
+            else:
+                select_statement = f"""
+                    SELECT DISTINCT urls.url, urls.title, urls.visit_count, (urls.last_visit_time/1000000 + (strftime('%s', '1601-01-01')))
+                    FROM urls, visits
+                    WHERE urls.id = visits.url AND
+                    urls.title IS NOT NULL AND
+                    urls.title != '' order by last_visit_time DESC; """
+            cursor.execute(select_statement)
+            r = cursor.fetchall()
+            res.extend(r)
+            os.remove(os.path.join(history_db, 'History.db'))  # Delete History file in current dir
+            os.remove(os.path.join(history_db, 'History.db-shm'))
+            os.remove(os.path.join(history_db, 'History.db-wal'))
+        except sqlite3.Error as e:
+            raise Exception(f"SQL Error: {e}")
+        return res
+    
+    @staticmethod
+    def format(data:tuple):
+        """
+        将SQL输出结果格式化为dict
+        """
+        items = list()
+        def timeFormat(timestamp):
+            """
+            时间戳转换
+            """
+            # 将时间戳转换为 datetime 对象
+            dt_object = datetime.datetime.fromtimestamp(int(timestamp))
+            # 将 datetime 对象格式化为指定的时间字符串
+            formatted_time = dt_object.strftime('%Y-%m-%d %H:%M:%S')
+            return formatted_time
+
+        for line in data:
+            _title = line[1]
+            link = line[0]
+            visit_count = line[2]
+            _time = line[3]
+            visit_time = timeFormat(_time)
+            line_dict = {
+            'type':'default',
+            'title':_title,
+            'subtitle':f'{visit_count} visits on {visit_time} - {link}',
+            'arg':link
+            }
+            items.append(line_dict)
+    
+        return items
+    
+
+    @staticmethod
+    def show(items):
+        re_list = []
+        for item in items:
+            if len(sys.argv) == 2:
+                target = sys.argv[1]
+                _title = item['title']
+                link = item['arg']
+                if re.search(target, _title, flags=re.IGNORECASE):
+                    re_list.append(item)
+                elif re.search(target, link, flags=re.IGNORECASE):
+                    re_list.append(item)
+            else:
+                re_list.append(item)
+
+        result_json = json.dumps({"items":re_list}, ensure_ascii=False)
+        return result_json
+            
+
+
+
+def main():
+    # Get History file path
+    history_db_path = os.environ['HOME'] + '/Library/Safari/History.db'
+    # Get History file content
+    history_db = History.sql(history_db_path)
+    items = History.format(history_db)
+    output = History.show(items)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
+    
