05b25ff98fff13c9e4b36b1e218adbd60f4cdb2f
BlackCCCat <360252749zzx@gmail.com>
2023-10-08 20:41:19 +0800

    适配从话题进入的功能

--- hot-news/scripts/v2ex.py
+++ hot-news/scripts/v2ex.py
@@ -1,17 +1,25 @@
-from getnews import HotNews
-from alfred_show import alfred
 import sys
 
+from getnews import HotNews
+from alfred_show import alfred
+
 def main():
-    try:
-        n = int(sys.argv[1])
-    except:
-        n = 10
+    n = 10
+    tab = 'hot'
+    
+    if len(sys.argv) == 3:
+        n = int(sys.argv[2])
+        tab = sys.argv[1]
+    if len(sys.argv) == 2:
+        try:
+            n = int(sys.argv[1])
+        except:
+            tab = sys.argv[1]
+
 
     hn = HotNews(n)
 
-
-    v2ex_news = hn.getV2exNews()
+    v2ex_news = hn.getV2exNews(tab=tab)
     print(alfred(v2ex_news))
 
 
